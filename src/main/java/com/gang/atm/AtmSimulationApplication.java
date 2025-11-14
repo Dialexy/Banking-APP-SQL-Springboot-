b7517ca21f089cc393fa780ae6f2cc31ab003e26
Dialexy <filiperamos212@gmail.com>
2025-11-14 16:55:18 +0000

    inital commit

---
+++ src/main/java/com/gang/atm/AtmSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.gang.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gang/atm/AtmSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gang.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
